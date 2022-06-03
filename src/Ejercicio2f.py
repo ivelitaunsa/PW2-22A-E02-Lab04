8cf3bd9fe901001531f8f5a6900733f2ed67e1ae
Eberth Wilfredo Azurin Zuñiga <eazurin@unsa.edu.pe>
2022-06-03 18:05:11 -0500

    Creando la serie de cuadrados 1 y 2, la primera empieza con un blanco y la segunda con un negro

--- src/Ejercicio2f.py
+++ src/Ejercicio2f.py
@@ -1,2 +1,11 @@
 from interpreter import draw
 from chessPictures import *
+
+myPicture1 = square
+myPicture1 = myPicture1.join(square.negative())
+myPicture1 = myPicture1.horizontalRepeat(3)
+
+myPicture2 = square.negative()
+myPicture2 = myPicture2.join(square)
+myPicture2 = myPicture2.horizontalRepeat(3)
+
