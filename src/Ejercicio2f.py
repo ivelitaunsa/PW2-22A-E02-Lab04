42221bb4a6abbb2f67fece182c2c53c6a4b95e17
Eberth Wilfredo Azurin Zuñiga <eazurin@unsa.edu.pe>
2022-06-03 18:07:26 -0500

    Uniendo las figuras, colocando la figura 1 arriba

--- src/Ejercicio2f.py
+++ src/Ejercicio2f.py
@@ -9,3 +9,6 @@ myPicture2 = square.negative()
 myPicture2 = myPicture2.join(square)
 myPicture2 = myPicture2.horizontalRepeat(3)
 
+myPictureFinal = myPicture2.up(myPicture1)
+draw(myPictureFinal)
+
