957521b84344df0d65cb2d938e8d60a1fe2a7d18
fluchi <fcardenasm@unsa.edu.pe>
2022-06-03 19:08:36 -0500

    creo variables myPicture y squreChange este ultimo va cambiando en cada vuelta del for,para tener cuadrados intercalados

--- src/Ejercicio2d.py
+++ src/Ejercicio2d.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+myPicture = square
+squareChange = square
+for i in range(7):
+  squareChange = squareChange.negative()
+  myPicture = myPicture.join(squareChange)
+draw(myPicture)
