0184cde253deffa18b23bf16e564cc065ad82e6d
fluchi <fcardenasm@unsa.edu.pe>
2022-06-03 22:52:00 -0500

    resumo codigo usando horizontalrepeat, join y negative

--- src/Ejercicio2d.py
+++ src/Ejercicio2d.py
@@ -1,9 +1,4 @@
 from interpreter import draw
 from chessPictures import *
 
-myPicture = square
-squareChange = square
-for i in range(7):
-  squareChange = squareChange.negative()
-  myPicture = myPicture.join(squareChange)
-draw(myPicture)
+draw(square.join(square.negative()).horizontalRepeat(3))
