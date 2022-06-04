ed8f4faef51d95b03a9881c0b0e510eede1205a0
fluchi <fcardenasm@unsa.edu.pe>
2022-06-03 22:49:06 -0500

    resumo codigo usando horizontalrepeat

--- src/Ejercicio2c.py
+++ src/Ejercicio2c.py
@@ -1,7 +1,4 @@
 from interpreter import draw
 from chessPictures import *
 
-myPicture = queen
-for i in range(3):
-  myPicture = myPicture.join(queen)
-draw(myPicture)
+draw(queen.horizontalRepeat(3))
